02735637618330473a0621d5982722bf0a0f7082
Christian GM <christian_magdaleno@comunidad.unam.mx>
2022-05-09 15:42:47 -0500

    Panaderia desde cero

---
+++ AricmaPanaderia/src/main/java/unam/infovi/aricma/PanaderiaApplication.java
@@ -0,0 +1,13 @@
+package unam.infovi.aricma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanaderiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanaderiaApplication.class, args);
+	}
+
+}
