75474e9b526c819916e361597245b81ad5335011
Christian GM <christian_magdaleno@comunidad.unam.mx>
2022-05-09 15:51:53 -0500

    Comentario App Desde Cero

--- AricmaPanaderia/src/main/java/unam/infovi/aricma/PanaderiaApplication.java
+++ AricmaPanaderia/src/main/java/unam/infovi/aricma/PanaderiaApplication.java
@@ -9,5 +9,18 @@ public class PanaderiaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PanaderiaApplication.class, args);
 	}
+        /**
+         * Esta es el "main" de la aplicación Spring Boot, aqui se cargará
+         * la misma de manera que se va a ejecutar en el momento que se levante 
+         * la aplicación.
+         * 
+         * EL namespace llamado "unam.infovi.aricma" hace referencia al Group ID
+         * que se pide con el initializer de Spring, es decir que este es el nombre
+         * del grupo del equipo o la organizacion con el que estamos trabajando
+         * 
+         * Y lo seccionamos en unam, mismo que dentro tiene la asignatura infoVi
+         * y que dentro contiene al equipo de aricma, el cual contiene el resto de 
+         * paquetes / capas
+         */
 
 }
